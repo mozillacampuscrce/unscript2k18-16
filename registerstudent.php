c34334290e2558982811ae4af9011e7a3c83f8fe
saurabhsingh0 <singhsaurav727@gmail.com>
2018-03-25 03:08:58 +0530

    Update registerstudent.php

--- registerstudent.php
+++ registerstudent.php
@@ -11,7 +11,7 @@
 	$mobileno=$_POST['mobileno'];
 echo $name ."   "  .$username."  ".$password;
 
-	$sql="INSERT INTO `students` (`username`,`password`,`name`,`email_id`,`class`,`division`,`branch`,`roll_no`,`contact_no`) VALUES('$username','$password','$name','$userid','$class','$division','$branch','$rollno','$mobileno')";
+$sql="INSERT INTO `students` (`username`,`password`,`name`,`email_id`,`class`,`division`,`branch`,`roll_no`,`contact_no`) VALUES('$username','$password','$name','$userid','$class','$division','$branch','$rollno','$mobileno')";
 
 	$result=mysqli_query($conn,$sql);
 	if ($result) {
