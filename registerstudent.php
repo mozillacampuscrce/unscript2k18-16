b2ae6d1fc91474c26c46d64a9acd52cd6322dd32
jigarsoni181 <34765103+jigarsoni181@users.noreply.github.com>
2018-03-25 02:09:48 +0530

    Update registerstudent.php

--- registerstudent.php
+++ registerstudent.php
@@ -9,7 +9,6 @@
 	$rollno=$_POST['rollno'];
 	$userid=$_POST['emailid'];
 	$mobileno=$_POST['mobileno'];
-
 echo $name ."   "  .$username."  ".$password;
 
 	$sql="INSERT INTO `students` (`username`,`password`,`name`,`email_id`,`class`,`division`,`branch`,`roll_no`,`contact_no`) VALUES('$username','$password','$name','$userid','$class','$division','$branch','$rollno','$mobileno')";
